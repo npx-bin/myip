57e0fcf6cff8a600b16cad27210985d375195956
K.C.Ashish Kumar <kcak11@gmail.com>
2019-12-12 11:51:06 +0530

    Update index.js

--- exec/index.js
+++ exec/index.js
@@ -2,23 +2,23 @@ var os = require('os');
 var ifaces = os.networkInterfaces();
 
 module.exports = function() {
-  let hosts = [];
-  Object.keys(ifaces).forEach(function(ifname) {
-      var alias = 0;
-      ifaces[ifname].forEach(function(iface) {
-          if ('IPv4' !== iface.family || iface.internal !== false) {
-              // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-              return;
-          }
-          if (alias >= 1) {
-              // this single interface has multiple ipv4 addresses
-              hosts.push(iface.address);
-          } else {
-              // this interface has only one ipv4 adress
-              hosts.push(iface.address);
-          }
-          ++alias;
-      });
-  });
-  return hosts;
+    let hosts = [];
+    Object.keys(ifaces).forEach(function(ifname) {
+        var alias = 0;
+        ifaces[ifname].forEach(function(iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                hosts.push(iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                hosts.push(iface.address);
+            }
+            ++alias;
+        });
+    });
+    return hosts;
 };
