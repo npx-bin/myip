80ba6f96b9f65b64d02121b275e4355c69e8749d
K.C.Ashish Kumar <kcak11@gmail.com>
2019-12-12 10:52:13 +0530

    Add files via upload

---
+++ cli/index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const myip = require('../exec');
+const hosts=myip();
+const printLine = () => {
+  let str="";
+  for(let i=0;i<80;++i){
+    str+="~";
+  }
+  console.log(str);
+};
+console.log();
+console.log("Following are the IP Addresses for your machine:");
+console.log();
+printLine();
+hosts.forEach(host => {
+  console.log(host + "     | http://"+host+" (or) https://"+host );
+});
+printLine();
+console.log("For more cool stuff, visit: https://github.com/kcak11");
+console.log();
