85c62ff2517bc977f32887e64b58e8ae60d5ff9f
Ashish Kumar KC <ashish-kumar.kothapally-chandrakanth@broadcom.com>
2025-07-29 05:46:39 +0530

    Release 5.0.4

---
+++ exec/external.js
@@ -0,0 +1,20 @@
+/*
+ * Fetch External(Public) IP
+ * Author: @kcak11
+ *
+ * The external IP information is fetched from:
+ * https://cors.kcak11.com/?myip=yes
+**/
+
+const axios = require('axios');
+
+module.exports = function (action) {
+    let promise = axios.get("https://cors.kcak11.workers.dev/?myip=yes");
+    return new Promise((resolve, reject) => {
+        promise.then((response) => {
+            resolve(response.data);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+};
