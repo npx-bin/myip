e1aeec95998d54185a65dfdd58436d496f080c1d
Ashish Kumar KC <ashish-kumar.kothapally-chandrakanth@broadcom.com>
2025-09-15 17:18:45 +0530

    Initial commit

---
+++ utils/io.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+module.exports = {
+  blockInput: function () {
+    rl.pause();
+  }
+};
