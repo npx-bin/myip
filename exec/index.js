257eca3ab7dc66a58aacd11897b94939119798f2
K.C.Ashish Kumar <ashish-kumar.kothapally-chandrakanth@broadcom.com>
2026-04-13 06:51:12 +0530

    Initial commit

---
+++ exec/index.js
@@ -0,0 +1,21 @@
+/*
+ * Fetch the Internal IP addresses for the machine.
+ * Author: @kcak11
+**/
+
+const os = require('os');
+const ifaces = os.networkInterfaces();
+
+module.exports = function () {
+    let hosts = [];
+    Object.keys(ifaces).forEach((ifname) => {
+        ifaces[ifname].forEach((iface) => {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+            hosts.push(iface.address);
+        });
+    });
+    return hosts;
+};
